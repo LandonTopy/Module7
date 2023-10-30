71cb9056aefdb8d56c28f4abf65465ae56962220
LandonTopy <landontopy@gmail.com>
2023-10-30 09:44:30 -0400

    Hi

--- src/Task2.java
+++ src/Task2.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Task2 {
+public class Task2 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 5; i++){
+            for(int j = 1; j<= i ; j++)
+                System.out.print("*");
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for (int i = 5; i >= 1; i--){
+            for(int j = 1; j <= i ; j++)
+                System.out.print("*");
+            System.out.println("");
+        }
+        System.out.println("\n");
+        for (int i = 1; i <= 5; i++){
+            for(int j = 1; j<= 5; j++)
+                System.out.print("*");
+            System.out.println("");
+        }
+    }
 }
