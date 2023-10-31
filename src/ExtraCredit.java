7ef5a6d9ecae7fc95156b0f9c27ab4e8153de027
LandonTopy <landontopy@gmail.com>
2023-10-31 09:06:32 -0400

    Hey

---
+++ src/ExtraCredit.java
@@ -0,0 +1,30 @@
+public class ExtraCredit {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 3; i++){
+            for(int j = 1; j<= 17 ; j++)
+                System.out.print("*");
+            System.out.print("\n");
+        }
+        for (int i = 1; i <= 2; i++){
+            for(int j = 1; j<= 6 ; j++)
+                System.out.print("*");
+            System.out.print("\n");
+        }
+        for (int i = 1; i <= 3; i++){
+            for(int j = 1; j<= 12 ; j++)
+                System.out.print("*");
+            System.out.print("\n");
+        }
+        for (int i = 1; i <= 2; i++){
+            for(int j = 1; j<= 6 ; j++)
+                System.out.print("*");
+            System.out.print("\n");
+        }
+        for (int i = 1; i <= 3; i++){
+            for(int j = 1; j<= 17 ; j++)
+                System.out.print("*");
+            System.out.print("\n");
+        }
+    }
+}
